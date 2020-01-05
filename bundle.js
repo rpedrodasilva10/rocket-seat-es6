32408c9eda8f3b35d259d8d96ea7328e7902f11b
Renan Pedro <rpedrodasilva10@gmail.com>
2020-01-05 15:09:50 -0300

    Adds exercicises 1 and 2

--- bundle.js
+++ bundle.js
@@ -1,5 +1,17 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -30,4 +42,76 @@ var MinhaLista = new TodoList();
 
 document.getElementById('novotodo').onclick = function () {
   MinhaLista.addTodo();
-};
+}; // Exercicios
+//EX1
+
+
+var Usuario =
+/*#__PURE__*/
+function () {
+  function Usuario(email, senha) {
+    var admin = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+    _classCallCheck(this, Usuario);
+
+    this.email = email;
+    this.senha = senha;
+    this.admin = admin;
+  }
+
+  _createClass(Usuario, [{
+    key: "isAdmin",
+    value: function isAdmin() {
+      return this.admin;
+    }
+  }]);
+
+  return Usuario;
+}();
+
+var Admin =
+/*#__PURE__*/
+function (_Usuario) {
+  _inherits(Admin, _Usuario);
+
+  function Admin(email, senha) {
+    _classCallCheck(this, Admin);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Admin).call(this, email, senha, true));
+  }
+
+  return Admin;
+}(Usuario);
+
+var User1 = new Usuario('email@teste.com', 'senha123');
+var Adm1 = new Admin('email@teste.com', 'senha123');
+console.log(User1);
+console.log(Adm1);
+console.log(User1.isAdmin()); // false
+
+console.log(Adm1.isAdmin()); // true
+//exercicios 2
+//ex2.1
+
+var usuarios = [{
+  nome: 'Diego',
+  idade: 23,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Gabriel',
+  idade: 15,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Lucas',
+  idade: 30,
+  empresa: 'Facebook'
+}];
+var idades = usuarios.map(function (item) {
+  return item['idade'];
+});
+console.log(idades); //ex2.2
+
+var users = usuarios.filter(function (item) {
+  return item.idade >= 18 && item.empresa === "Rocketseat";
+});
+console.log(users);
--- main.js
+++ main.js
@@ -14,3 +14,46 @@ const MinhaLista = new TodoList();
 document.getElementById('novotodo').onclick = function () {
     MinhaLista.addTodo();
 }
+
+// Exercicios
+//EX1
+class Usuario {
+    constructor(email, senha, admin = false) {
+        this.email = email;
+        this.senha = senha;
+        this.admin = admin;
+    }
+    isAdmin() {
+        return this.admin;
+    }
+}
+
+class Admin extends Usuario {
+    constructor(email, senha) {
+        super(email, senha, true);
+    }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+console.log(User1);
+console.log(Adm1);
+
+console.log(User1.isAdmin()) // false
+console.log(Adm1.isAdmin()) // true
+
+//exercicios 2
+//ex2.1
+const usuarios = [
+    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+];
+
+const idades = usuarios.map(item => item['idade']);
+console.log(idades);
+
+//ex2.2
+const users = usuarios.filter(item => item.idade >= 18 && item.empresa === "Rocketseat");
+console.log(users);
+
